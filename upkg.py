8763847ac6f535d34a1f0b43aedd98d27e269ba0
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2023-03-28 19:20:05 -0400

    Untested initial commit...
    Untested.

---
+++ upkg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#A simple little script to update all of your Python packages, your Arch packages, as well as pip! Written by Kaede <3
+
+import pkg_resources
+from subprocess import call
+
+print("\033[1;33 mThis tool will attempt to update the following Python scripts, as well as your Arch system:")
+
+tpkgs = [dist.project_name for dist in pkg_resources.working_set]
+pkgs = []
+for pkg in tpkgs:
+    if not pkg.startswith("-"):
+        print("\033[1;32 m"+pkg)
+        if pkg != "pip":
+            pkgs.append(pkg)
+input("Press Enter to continue...\n")
+call("python -m pip install --upgrade pip", shell=True)
+call("pip install --upgrade " + ' '.join(pkgs), shell=True)
+print("\033[1;33 m(Hopefully) \033[1;32 mSuccessfully updated all Python packages! Moving on to pacman -Syu!")
+try:
+    isPacman = call(['which', 'pacman'])
+    if isPacman == 0:
+        call("pacman -Syu", shell=True)
+    else:
+        print("Could not find pacman... you are likely running this on a non Arch environment, and that's perfectly fine if you're just updating Python packages!")
+except:
+    print("You are likely not on *NIX... unexpected issues may occur, please refrain from use of this script. (pacman -Syu was NOT ran :)")
